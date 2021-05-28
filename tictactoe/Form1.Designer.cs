4dad79cfd44464549bad379a5ab5ff3e0d5416a4
MYPC <MYPC@DESKTOP-38G0JLO>
2021-05-29 04:23:09 +0800

    422

--- tictactoe/Form1.Designer.cs
+++ tictactoe/Form1.Designer.cs
@@ -39,13 +39,17 @@
             this.btnnew = new System.Windows.Forms.Button();
             this.btna2 = new System.Windows.Forms.Button();
             this.btna1 = new System.Windows.Forms.Button();
+            this.drw = new System.Windows.Forms.Label();
+            this.btnex = new System.Windows.Forms.Button();
+            this.laby = new System.Windows.Forms.Label();
+            this.labx = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnc1
             // 
             this.btnc1.BackColor = System.Drawing.Color.Transparent;
             this.btnc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnc1.Location = new System.Drawing.Point(26, 301);
+            this.btnc1.Location = new System.Drawing.Point(26, 295);
             this.btnc1.Name = "btnc1";
             this.btnc1.Size = new System.Drawing.Size(92, 84);
             this.btnc1.TabIndex = 1;
@@ -56,7 +60,7 @@
             // 
             this.btnb1.BackColor = System.Drawing.Color.Transparent;
             this.btnb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnb1.Location = new System.Drawing.Point(26, 211);
+            this.btnb1.Location = new System.Drawing.Point(26, 205);
             this.btnb1.Name = "btnb1";
             this.btnb1.Size = new System.Drawing.Size(92, 84);
             this.btnb1.TabIndex = 2;
@@ -67,7 +71,7 @@
             // 
             this.btnc2.BackColor = System.Drawing.Color.Transparent;
             this.btnc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnc2.Location = new System.Drawing.Point(124, 300);
+            this.btnc2.Location = new System.Drawing.Point(124, 295);
             this.btnc2.Name = "btnc2";
             this.btnc2.Size = new System.Drawing.Size(92, 84);
             this.btnc2.TabIndex = 3;
@@ -78,7 +82,7 @@
             // 
             this.btnb2.BackColor = System.Drawing.Color.Transparent;
             this.btnb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnb2.Location = new System.Drawing.Point(124, 211);
+            this.btnb2.Location = new System.Drawing.Point(124, 205);
             this.btnb2.Name = "btnb2";
             this.btnb2.Size = new System.Drawing.Size(92, 84);
             this.btnb2.TabIndex = 4;
@@ -89,7 +93,7 @@
             // 
             this.btna3.BackColor = System.Drawing.Color.Transparent;
             this.btna3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btna3.Location = new System.Drawing.Point(222, 121);
+            this.btna3.Location = new System.Drawing.Point(222, 115);
             this.btna3.Name = "btna3";
             this.btna3.Size = new System.Drawing.Size(92, 84);
             this.btna3.TabIndex = 6;
@@ -100,7 +104,7 @@
             // 
             this.btnb3.BackColor = System.Drawing.Color.Transparent;
             this.btnb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnb3.Location = new System.Drawing.Point(222, 211);
+            this.btnb3.Location = new System.Drawing.Point(222, 205);
             this.btnb3.Name = "btnb3";
             this.btnb3.Size = new System.Drawing.Size(92, 84);
             this.btnb3.TabIndex = 7;
@@ -111,7 +115,7 @@
             // 
             this.btnc3.BackColor = System.Drawing.Color.Transparent;
             this.btnc3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnc3.Location = new System.Drawing.Point(222, 300);
+            this.btnc3.Location = new System.Drawing.Point(222, 295);
             this.btnc3.Name = "btnc3";
             this.btnc3.Size = new System.Drawing.Size(92, 84);
             this.btnc3.TabIndex = 8;
@@ -125,18 +129,19 @@
             this.btnnew.FlatAppearance.BorderSize = 5;
             this.btnnew.Font = new System.Drawing.Font("Bell MT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnnew.ForeColor = System.Drawing.Color.Black;
-            this.btnnew.Location = new System.Drawing.Point(173, 390);
+            this.btnnew.Location = new System.Drawing.Point(26, 391);
             this.btnnew.Name = "btnnew";
             this.btnnew.Size = new System.Drawing.Size(141, 41);
             this.btnnew.TabIndex = 9;
             this.btnnew.Text = "NEW GAME";
             this.btnnew.UseVisualStyleBackColor = false;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
             // 
             // btna2
             // 
             this.btna2.BackColor = System.Drawing.Color.Transparent;
             this.btna2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btna2.Location = new System.Drawing.Point(124, 121);
+            this.btna2.Location = new System.Drawing.Point(124, 115);
             this.btna2.Name = "btna2";
             this.btna2.Size = new System.Drawing.Size(92, 84);
             this.btna2.TabIndex = 5;
@@ -147,12 +152,60 @@
             // 
             this.btna1.BackColor = System.Drawing.Color.Transparent;
             this.btna1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btna1.Location = new System.Drawing.Point(26, 121);
+            this.btna1.Location = new System.Drawing.Point(26, 115);
             this.btna1.Name = "btna1";
             this.btna1.Size = new System.Drawing.Size(92, 84);
             this.btna1.TabIndex = 10;
             this.btna1.UseVisualStyleBackColor = false;
             this.btna1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // drw
+            // 
+            this.drw.BackColor = System.Drawing.Color.Transparent;
+            this.drw.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drw.ForeColor = System.Drawing.Color.Snow;
+            this.drw.Location = new System.Drawing.Point(118, 65);
+            this.drw.Name = "drw";
+            this.drw.Size = new System.Drawing.Size(147, 29);
+            this.drw.TabIndex = 14;
+            this.drw.Text = "Draws:";
+            // 
+            // btnex
+            // 
+            this.btnex.BackColor = System.Drawing.Color.Transparent;
+            this.btnex.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnex.FlatAppearance.BorderSize = 5;
+            this.btnex.Font = new System.Drawing.Font("Bell MT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnex.ForeColor = System.Drawing.Color.Black;
+            this.btnex.Location = new System.Drawing.Point(173, 390);
+            this.btnex.Name = "btnex";
+            this.btnex.Size = new System.Drawing.Size(141, 41);
+            this.btnex.TabIndex = 15;
+            this.btnex.Text = "EXIT";
+            this.btnex.UseVisualStyleBackColor = false;
+            this.btnex.Click += new System.EventHandler(this.btnex_Click);
+            // 
+            // laby
+            // 
+            this.laby.BackColor = System.Drawing.Color.Transparent;
+            this.laby.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.laby.ForeColor = System.Drawing.Color.Snow;
+            this.laby.Location = new System.Drawing.Point(199, 22);
+            this.laby.Name = "laby";
+            this.laby.Size = new System.Drawing.Size(140, 56);
+            this.laby.TabIndex = 13;
+            this.laby.Text = "Y Wins:";
+            // 
+            // labx
+            // 
+            this.labx.BackColor = System.Drawing.Color.Transparent;
+            this.labx.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labx.ForeColor = System.Drawing.Color.Snow;
+            this.labx.Location = new System.Drawing.Point(35, 22);
+            this.labx.Name = "labx";
+            this.labx.Size = new System.Drawing.Size(158, 56);
+            this.labx.TabIndex = 12;
+            this.labx.Text = "X Wins:";
             // 
             // TicTacToe
             // 
@@ -163,6 +216,10 @@
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(336, 443);
+            this.Controls.Add(this.btnex);
+            this.Controls.Add(this.drw);
+            this.Controls.Add(this.laby);
+            this.Controls.Add(this.labx);
             this.Controls.Add(this.btna1);
             this.Controls.Add(this.btnnew);
             this.Controls.Add(this.btnc3);
@@ -196,6 +253,10 @@
         private System.Windows.Forms.Button btnnew;
         private System.Windows.Forms.Button btna2;
         private System.Windows.Forms.Button btna1;
+        private System.Windows.Forms.Label drw;
+        private System.Windows.Forms.Button btnex;
+        private System.Windows.Forms.Label laby;
+        private System.Windows.Forms.Label labx;
     }
 }
 
--- tictactoe/Form1.cs
+++ tictactoe/Form1.cs
@@ -15,34 +15,103 @@ namespace tictactoe
         public TicTacToe()
         {
             InitializeComponent();
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            InitializeComponent();
+
         }
         public int player = 2;
-        public int turns = 0;
-        public int s1 = 0;
-        public int s2 = 0;
+        public int move = 0;
+        public int xwin = 0;
+        public int ywin = 0;
+        public int draw = 0;
 
         private void buttonClick(object sender, EventArgs e)
         {
             Button button = (Button)sender;
-            if (player % 2 == 0)
+            if (button.Text == "")
+                if (player % 2 == 0)
+                {
+                    button.Text = "X";
+                    player++;
+                    move++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    player++;
+                    move++;
+                }
             {
-                button.Text = "X";
-                player++;
-                turns++;
-            }
+                if (drawgame() == true)
+                {
 
-
-            else
-            {
-                button.Text = "O";
-                player++;
-                turns++;
-            }
-              
+                    MessageBox.Show("Draw");
+                    draw++;
+                    Restart();
+                }
+                if (Wingame() == true)
+                {
+                    if (button.Text == "O")
+                    {
+                        MessageBox.Show("O WON");
+                        ywin++;
+                        Restart();  
+                    }
+                    else
+                        MessageBox.Show("X WON");
+                        xwin++;
+                        Restart();
+                }
             }
         }
-   }
+        private void btnex_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labx.Text = "X Wins: " + xwin;
+            laby.Text = "Y Wins:" + ywin;
+            drw.Text = "Draw:" + draw;
+        }
+        void Restart()
+        {
+            player = 2;
+            move = 0;
+            btna1.Text = btna2.Text = btna3.Text = btnb1.Text = btnb2.Text = btnb3.Text = btnc1.Text = btnc2.Text = btnc3.Text = "";
+        }
+
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            Restart();
+        }
+        bool drawgame()
+        {
+            if (move == 9)
+                return true;
+            else
+                return false;
+        }
+        bool Wingame()
+        {
+            if ((btna1.Text == btna2.Text) && (btna2.Text == btna3.Text) && btna1.Text != "")
+                return true;
+            else if ((btnb1.Text == btnb2.Text) && (btnb2.Text == btnb3.Text) && btnb1.Text != "")
+                return true;
+            else if ((btnc1.Text == btnc2.Text) && (btnc2.Text == btnc3.Text) && btnc1.Text != "")
+                return true;
+
+            if ((btna1.Text == btnb1.Text) && (btnb1.Text == btnc1.Text) && btna1.Text != "")
+                return true;
+            else if ((btna2.Text == btnb2.Text) && (btnb2.Text == btnc2.Text) && btna2.Text != "")
+                return true;
+            else if ((btna3.Text == btnb3.Text) && (btnb3.Text == btnc3.Text) && btna3.Text != "")
+                return true;
+
+            if ((btna1.Text == btnb2.Text) && (btnb2.Text == btnc3.Text) && btna1.Text != "")
+                return true;
+            else if ((btnb1.Text == btnb2.Text) && (btnb2.Text == btnc2.Text) && btnb1.Text != "")
+                return true;
+            else
+                return false;
+        }
+    }
+}
